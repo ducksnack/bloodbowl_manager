a1d8dca3a3d8a3e4a76fa151053f5fcf55f1de1e
ducksnack <askesolesen@gmail.com>
2025-01-17 12:29:21 +0100

    updated admin.py

--- bloodbowl_league_manager/league_manager/admin.py
+++ bloodbowl_league_manager/league_manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import League, Team, Player, PlayerType, Faction, Match, Touchdown
+from .models import League, Team, Player, PlayerType, Faction, Match, Touchdown, PassCompletion, Casualty, Interception, MostValuablePlayer, InjuryType, Injury
 
 admin.site.register(League)
 admin.site.register(Team)
@@ -8,3 +8,9 @@ admin.site.register(PlayerType)
 admin.site.register(Faction)
 admin.site.register(Match)
 admin.site.register(Touchdown)
+admin.site.register(PassCompletion)
+admin.site.register(Casualty)
+admin.site.register(Interception)
+admin.site.register(MostValuablePlayer)
+admin.site.register(InjuryType)
+admin.site.register(Injury)
