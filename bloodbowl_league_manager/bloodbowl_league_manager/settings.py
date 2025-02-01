5c1a49af72f0eea834ddd93e651875a6d1f2083e
ducksnack <askesolesen@gmail.com>
2025-02-01 08:40:55 +0100

    playing around with settings.py to get statics files to work on pythonanywhere

--- bloodbowl_league_manager/bloodbowl_league_manager/settings.py
+++ bloodbowl_league_manager/bloodbowl_league_manager/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "league_manager/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
